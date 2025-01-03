c55420bc24b06f5c674cf15cdb9a3d93cccc22eb
EuronRamadani <73657377+EuronRamadani@users.noreply.github.com>
2025-01-03 09:49:22 +0100

    fixed the purchase implementation

---
+++ frontends/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/P2P":["static\u002Fchunks\u002Fpages\u002FP2P.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FP2P","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
